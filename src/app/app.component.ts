b63a28f3454003ace1ad77981a003a895948fe50
Martin Romero <mr.romero@uniandes.edu.co>
2025-04-26 01:40:56 -0500

    udpate templates for data mapping and principal template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PlantasComponent } from './plantas/plantas.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PlantasService } from './plantas/plantas.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [HttpClientModule, PlantasComponent], // Importa el componente aquí
+  providers: [PlantasService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'parcial';
+  title = 'Vivero El Otoño';
 }
