af4d893504869e70ed58dc3fd9489dab7427d295
Martin Romero <mr.romero@uniandes.edu.co>
2025-04-26 01:33:25 -0500

    add service for plantas

---
+++ src/app/plantas/plantas.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Planta } from './planta';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PlantasService {
+
+    private apiUrl = environment.apiUrl;
+
+    constructor(private http: HttpClient) { }
+
+    getPlantas(): Observable<Planta[]> {
+        return this.http.get<Planta[]>(this.apiUrl);
+    }
+
+}
