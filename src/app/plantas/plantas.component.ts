3aa8455184f04f5ca5fb947d80c67ad4e4ecd8aa
Martin Romero <mr.romero@uniandes.edu.co>
2025-04-26 01:36:23 -0500

    add service consume in component

--- src/app/plantas/plantas.component.ts
+++ src/app/plantas/plantas.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PlantasService } from './plantas.service';
+import { Planta } from './planta';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-plantas',
   templateUrl: './plantas.component.html',
-  styleUrls: ['./plantas.component.css']
+  styleUrls: ['./plantas.component.css'],
+  imports: [CommonModule],
 })
 export class PlantasComponent implements OnInit {
 
-  constructor() { }
+  plantas: Planta[] = [];
+  
+  constructor(private plantaService: PlantasService) { }
 
   ngOnInit() {
+    this.plantaService.getPlantas().subscribe(data => {
+      this.plantas = data;
+    }
+    );
   }
-
 }
